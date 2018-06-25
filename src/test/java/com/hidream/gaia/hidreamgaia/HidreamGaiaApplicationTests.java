399a0f0bb6e2c8c15ebdfacd45cf45fd61680d38
liujiao <liujiao@baokeyun.com>
2018-06-25 11:48:55 +0800

    init

---
+++ src/test/java/com/hidream/gaia/hidreamgaia/HidreamGaiaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hidream.gaia.hidreamgaia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HidreamGaiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
